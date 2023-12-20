263eacd3adee94fc3d3dbcfcdf1160595cabe3d9
Dmitrii Kupriunin <kupriunin@gmail.com>
2023-12-20 18:29:54 +0100

    fe(2): validation to a register form

---
+++ fe/src/components/InputErrorMessage/InputErrorMessage.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { colors } from '../../shared/colors';
+
+export const InputErrorMessage = styled.p`
+  margin: 5px 0 0 0;
+  font-size: 12px;
+  color: ${colors.red.primary};
+`;
